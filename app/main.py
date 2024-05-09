a58933e38c26548d0ed7958b2dd27e674fc2b0c0
Marko Djordjic <marko.djordjic@outlook.com>
2024-05-09 09:23:59 +0200

    feat(excecise): Addition of the path parameters.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,75 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class System(BaseModel):
+    name: str
+    pre_processor: str | None
+    predictor: str
+    post_processor: str | None
+
+system_specification_values = {
+    "name": "Linear Regression",
+    "pre_processor": "StandardScaler",
+    "predictor": "LinearRegression",
+    "post_processor": None
+}
+
+system = System(**system_specification_values)
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    """Test
+    """Path function
 
     """    
     
     return {"message": "Hello wild"}
+
+@app.get("/items/{item}")
+async def path_parameter(item_id):
+    """Path function
+
+    """    
+    
+    return {"message": item_id}
+
+@app.get("/items/{item_id}")
+async def path_parameter_wh_type(item_id: int):
+    """Path function
+
+    Parameters
+    ----------
+    item_id : int
+        _description_
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
+    
+    return {"item_id": item_id}
+
+
+@app.get("/models/{system_specification}")
+async def get_model_specification(system_specification: System):
+
+    if system_specification.pre_processor is not None:
+
+        pre_status = f'System: {system_specification.name} requires pre-processor {system_specification.pre_processor}'
+
+    if system_specification.post_processor is not None:
+
+        post_status = f'System: {system_specification.name} requires post-processor {system_specification.pre_processor}'
+
+
+    return {"report": pre_status + r'\n' + post_status}
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+
+    return {"file_path": file_path}
---
+++ app/path_parameters.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+fake_items_db = [
+    {"item_name": "Foo"}, 
+    {"item_name": "Bar"}, 
+    {"item_name": "Baz"}
+]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    """_summary_
+
+    """
+    
+    return fake_items_db[skip : skip + limit]
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, query: str | None = None):
+
+    if query is not None:
+        
+        return {"item_id": item_id, "query": query}
+
+    return {"item_id": item_id}
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, user_name: str, query: str | None = None, short: bool = False
+):
+    item = {"item_id": user_id, "owner_id": user_name}
+    if query:
+        item.update({"query": query})
+    if short:
+        item.update(
+            {"short_name": user_name.upper()[0:3]}
+        )
+    return item
