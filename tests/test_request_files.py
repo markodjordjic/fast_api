e6388ee711975ddd86f1e74c7dc7b39ae3b443aa
Marko Djordjic <marko.djordjic@outlook.com>
2024-05-16 17:12:40 +0200

    feat(request_files): First tests passing.

---
+++ tests/test_request_files.py
@@ -0,0 +1,57 @@
+import unittest
+import csv
+import io
+from fastapi import status
+from fastapi.testclient import TestClient
+from app.request_files import app
+
+
+def create_file():
+
+    content = [
+        ['This', 'is', 'a', 'simple', 'text', 'file.'],
+        ['It', 'has', 'two', 'sentences.']
+    ]
+
+    output = io.StringIO()
+
+    writer = csv.writer(output)
+
+    writer.writerow(content)
+
+    return output.getvalue()
+
+class TestExercise(unittest.TestCase):
+
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+ 
+    @classmethod
+    def setUp(self) -> None:
+        self.app = TestClient(app=app)
+        self.test_data = create_file()
+
+
+    def test_create_file(self) -> None:
+
+        expected = {'file_size': 87}
+
+        response = self.app.post("/files/", data={'file':self.test_data})
+
+        actual = response.json()
+
+    
+        self.assertEqual(actual, expected)
+
+
+def test_suite():
+
+    suite = unittest.TestSuite()
+    suite.addTest(TestExercise('test_create_file'))
+
+    return suite
+
+if __name__ == '__main__':
+
+    runner = unittest.TextTestRunner()
+    runner.run(test_suite())
