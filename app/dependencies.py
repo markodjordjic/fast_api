821fd8e73e2f1882a07e0ba6bf20063825649a7a
Marko Djordjic <marko.djordjic@outlook.com>
2024-05-22 10:59:47 +0200

    feat(dependencies): Initial setup.

---
+++ app/dependencies.py
@@ -0,0 +1,16 @@
+from typing import Annotated
+
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+
+@app.post("/files/")
+async def create_file(file: Annotated[bytes, File()]):
+    
+    return {"file_size": len(file)}
+
+@app.post("/upload_file/")
+async def create_upload_file(file: Annotated[UploadFile, File()]):
+    
+    return {"filename": file.filename}
