9beb4e9a34a967a416072d532916e26ebc0a5716
Marko Djordjic <marko.djordjic@outlook.com>
2024-05-24 14:58:45 +0200

    feat(dependencies): Restructuring of the files.

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -2,9 +2,10 @@ import unittest
 from copy import deepcopy
 from fastapi.testclient import TestClient
 from app.dependecies.dependencies import app
+from app.dependecies.sub_dependencies import app
 
 
-class TestExercise(unittest.TestCase):
+class TestDependencies(unittest.TestCase):
 
     def __init__(self, methodName: str = "runTest") -> None:
         super().__init__(methodName)
@@ -55,12 +56,50 @@ class TestExercise(unittest.TestCase):
         self.assertEqual(actual, expected)
 
 
+class TestSubDependencies(unittest.TestCase):
+
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+ 
+    def setUp(self) -> None:
+        self.app = TestClient(app=app)
+
+    def test_read_query_wh_query(self) -> None:
+
+        expected = {
+            'q_or_cookie': 'test query'
+        }
+
+        parameters = {
+            'q': 'test query'
+        }
+
+        response = self.app.get("/items/", params=parameters)
+
+        actual = response.json()
+    
+        self.assertEqual(actual, expected)
+
+    def test_read_query_wo_query(self) -> None:
+
+        expected = {
+            'q_or_cookie': None
+        }
+
+        response = self.app.get("/items/")
+
+        actual = response.json()
+    
+        self.assertEqual(actual, expected)
 
 def test_suite():
 
     suite = unittest.TestSuite()
-    suite.addTest(TestExercise('test_read_items'))
-    suite.addTest(TestExercise('test_read_items_wh_commons'))
+    suite.addTest(TestDependencies('test_read_items'))
+    suite.addTest(TestDependencies('test_read_items_wh_commons'))
+    suite.addTest(TestSubDependencies('test_read_query_wh_query'))
+    suite.addTest(TestSubDependencies('test_read_query_wo_query'))
+
  
     return suite
 
