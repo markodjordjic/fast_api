316108ed7419e81e240e57acad7e9d555f061cbf
Marko Djordjic <marko.djordjic@outlook.com>
2024-05-27 00:38:21 +0200

    feat(jwt): Get user and pw with JWT.

--- tests/test_security.py
+++ tests/test_security.py
@@ -3,7 +3,7 @@ from fastapi.testclient import TestClient
 from app.security.base import app as base_app
 from app.security.get_current_user import app as user_app
 from app.security.get_user_and_pw import app as user_pw_app
-
+from app.security.get_user_and_pw_jwt import app as user_pw_jwt_app
 
 class TestBaseApp(unittest.TestCase):
 
@@ -94,6 +94,29 @@ class TestUserPWApp(unittest.TestCase):
     
         self.assertEqual(actual, expected)
 
+class TestUserPWJWTApp(unittest.TestCase):
+
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+ 
+    def setUp(self) -> None:
+        self.app = TestClient(app=user_pw_jwt_app)
+
+    def test_read_users_me(self) -> None:
+
+        expected = {'access_token': 'johndoe', 'token_type': 'bearer'}
+
+        payload = {
+            "username": "johndoe",
+            "password": "secret"            
+        }
+
+        response = self.app.get("/users/me")
+
+        actual = response.json()
+    
+        self.assertEqual(actual, expected)
+
 
 def test_suite():
 
@@ -103,6 +126,8 @@ def test_suite():
     suite.addTest(TestUserPWApp('test_login'))
     suite.addTest(TestUserPWApp('test_login_wrong_pw'))
     suite.addTest(TestUserPWApp('test_login_wrong_un'))
+    suite.addTest(TestUserPWApp('test_login_wrong_un'))
+    suite.addTest(TestUserPWJWTApp('test_read_users_me'))
     
     return suite
 
