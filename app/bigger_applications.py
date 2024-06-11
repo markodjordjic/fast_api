ea7ed96e07493774566f6ce70055d9595bf71f02
Marko Djordjic <marko.djordjic@outlook.com>
2024-06-11 11:48:40 +0200

    feat(routers): Introduction of routers.

--- app/bigger_applications.py
+++ app/bigger_applications.py
@@ -1,18 +1,22 @@
-from fastapi import APIRouter
+from fastapi import Depends, FastAPI
 
-router = APIRouter()
+from dependencies import get_query_token, get_token_header
+from app.internal import admin
+from app.routers import items, users
 
-
-@router.get("/users/", tags=["users"])
-async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
-
-
-@router.get("/users/me", tags=["users"])
-async def read_user_me():
-    return {"username": "fakecurrentuser"}
+app = FastAPI(dependencies=[Depends(get_query_token)])
 
 
-@router.get("/users/{username}", tags=["users"])
-async def read_user(username: str):
-    return {"username": username}
+app.include_router(users.router)
+app.include_router(items.router)
+app.include_router(
+    admin.router,
+    prefix="/admin",
+    tags=["admin"],
+    dependencies=[Depends(get_token_header)],
+    responses={418: {"description": "I'm a teapot"}}
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Bigger Applications!"}
